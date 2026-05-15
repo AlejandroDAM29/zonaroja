3a0ec9d026c9b02362d464169e0dd3b468817ca7
Alejandro Exposito <e962339@iberdrola.com>
2026-05-15 13:40:47 +0200

    hotfix version 1.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "alejandro.developer.zonaroja"
         minSdk = 24
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.6.0"
+        versionCode = 13
+        versionName = "1.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
